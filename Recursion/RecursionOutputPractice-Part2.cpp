f65770b88b8d5d6c45b7765cb90ef3ed4fec72fc
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-05-11 16:15:32 +0530

    Number to binary using recursion

--- Recursion/RecursionOutputPractice-Part2.cpp
+++ Recursion/RecursionOutputPractice-Part2.cpp
@@ -1,17 +1,29 @@
 #include<iostream>
 using namespace std;
 
-// Guess the OutPut
-int fun(int n){
-    if (n == 1)         // Base Condition
+// // Guess the OutPut
+// int fun(int n){
+//     if (n == 1)         // Base Condition
+//     {
+//         return 0;
+//     }
+//     return 1+fun(n/2);
+// }
+
+
+// Binary Conversion of a number using recursion
+void fun(int n){
+    if (n == 0)
     {
-        return 0;
+        return;
     }
-    return 1+fun(n/2);
+    fun(n/2);
+    cout << (n%2) << endl;
 }
 
 int main()
 {
-    cout<<fun(16);
+    // cout<<fun(16);
+    fun(7);
     return 0;
 }
